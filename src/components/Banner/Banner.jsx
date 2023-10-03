46786612a3bc579ee3b3570ba1777a99afa4a4a7
Moustapha <cmr.mous@gmail.com>
2023-10-03 17:56:17 +0200

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Banner = () => {
-  return <div>Banefener</div>;
+  return (
+    <div className="banner">
+      <img src="./src/assets/banner_btb.png" alt="" />
+    </div>
+  );
 };
 
 export default Banner;
