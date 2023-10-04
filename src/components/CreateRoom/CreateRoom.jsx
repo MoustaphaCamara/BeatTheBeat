c17be99c1fa80336e18dea406b939b6888271a4b
Askralos <moeungsiweil@gmail.com>
2023-10-04 10:32:36 +0200

    component: CreateRoom fini

--- src/components/CreateRoom/CreateRoom.jsx
+++ src/components/CreateRoom/CreateRoom.jsx
@@ -1,19 +1,27 @@
-import "./CreateRoom.scss"
+import "./CreateRoom.scss";
 
 const CreateRoom = () => {
-  return (
-    <div>
-      <div className="inputs">
-        <div className="side-create-room">
-          <h3>Pseudo</h3>
-          <div className="input-pseudo">
-            <input type="text" placeholder="Write here..." name="text" className="input" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+	return (
+		<div>
+			<div className="container-inputs">
+				<div className="side-create-room">
+					<h3>Pseudo</h3>
+					<div className="input-pseudo">
+						<input type="text" placeholder="pseudo" name="text" className="input" />
+						<button>Create Room</button>
+					</div>
+				</div>
 
+				<div className="side-join-room">
+					<h3>Rejoindre un salon</h3>
+					<div className="input-idRoom">
+						<input type="text" placeholder="#" name="text" className="input" />
+						<button>Join</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default CreateRoom
+export default CreateRoom;
