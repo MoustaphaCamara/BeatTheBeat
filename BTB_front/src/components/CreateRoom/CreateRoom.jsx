2c5f8ac3a17011ae625cd1db874855f14178c294
Moustapha <cmr.mous@gmail.com>
2023-10-06 15:51:12 +0200

    final push for socket branch

--- BTB_front/src/components/CreateRoom/CreateRoom.jsx
+++ BTB_front/src/components/CreateRoom/CreateRoom.jsx
@@ -2,48 +2,46 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import "./CreateRoom.scss";
 
-
-
 const CreateRoom = () => {
-	const [room, setRoom] = useState("");
-	const [author, setAuthor] = useState("")
+  const [room, setRoom] = useState("");
+  const [author, setAuthor] = useState("");
 
+  return (
+    <div>
+      <div className="container-inputs">
+        <div className="side-create-room">
+          <h3>Pseudo</h3>
+          <input
+            className="create_room_input"
+            type="text"
+            placeholder="pseudo"
+            name="text"
+            onChange={(event) => {
+              setAuthor(event.target.value);
+            }}
+          />
+        </div>
 
-	return (
-		<div>
-			<div className="container-inputs">
-				<div className="side-create-room">
-					<h3>Pseudo</h3>
-					<input
-							type="text"
-							placeholder="pseudo"
-							name="text"
-							onChange={(event) => {
-								setAuthor(event.target.value);
-							}}
-						/>
-				</div>
-
-				<div className="side-join-room">
-					<h3>Rejoindre un salon</h3>
-					<div className="input-pseudo">
-						<input
-							type="text"
-							placeholder="#"
-							name="text"
-							className={`input small`}
-							onChange={(event) => {
-								setRoom(event.target.value);
-							}}
-						/>
-						<Link to={`/waiting_room?room=${room}&author=${author}`}>
-							<button>Join</button>
-						</Link>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="side-join-room">
+          <h3>Rejoindre un salon</h3>
+          <div className="input-pseudo">
+            <input
+              type="text"
+              placeholder="#"
+              name="text"
+              className="input small create_room_input"
+              onChange={(event) => {
+                setRoom(event.target.value);
+              }}
+            />
+            <Link to={`/waiting_room?room=${room}&author=${author}`}>
+              <button className="create_room_button">Join</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateRoom;
