a4668d2b3fede18ea5cb518e359ca958e00f77e2
Askralos <moeungsiweil@gmail.com>
2023-10-06 09:18:01 +0200

    clone project + npm i

--- BTB_front/src/components/ui/InputButtonRoom/InputButton.jsx
+++ BTB_front/src/components/ui/InputButtonRoom/InputButton.jsx
@@ -1,12 +1,36 @@
-import "./InputButton.scss"
+/* eslint-disable react/prop-types */
+import { useEffect } from "react";
+import "./InputButton.scss";
 
-const InputPseudo = ({placeholder, button, size}) => {
-  return (
-    <div className="input-pseudo">
-      <input type="text" placeholder={placeholder} name="text" className={`input ${size}`} />
-      <button>{button}</button>
-    </div>
-  )
-}
+import io from "socket.io-client";
+import { Link, useLocation } from "react-router-dom";
+const socket = io.connect("http://localhost:3001");
 
-export default InputPseudo
+const Input = ({ placeholder, button, size }) => {
+    
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const idRoom = searchParams.get("room");
+    const player = searchParams.get("player");
+
+    const sendMessage = () => {
+        socket.emit("send_message", { message: "hello" });
+    };
+
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            alert(data.message);
+        });
+    }, [socket]);
+
+    return (
+        <div className="input-pseudo">
+            <input type="text" placeholder={placeholder} name="text" className={`input ${size}`} />
+            <Link to={`/waiting_room?room=${idRoom}&playerRoom=${player}`}>
+                <button onClick={sendMessage}>{button}</button>
+            </Link>
+        </div>
+    );
+};
+
+export default Input;
